1173292106afc4228280c66adc6a846dd730f99e
ninasolon <ninasolon@gmail.com>
2019-04-15 09:05:34 -0300

    imc e palindromo

--- imc-empoderado/assets/script.js
+++ imc-empoderado/assets/script.js
@@ -9,32 +9,37 @@ function calculoIMC(){
     var cssclass;
 
     if(resultado < 18.5){
-        text = "Uma deusa 🎵";
+        text = "O seu IMC é " + resultado.toFixed(2) + " - Uma deusa 🎵";
         gif = `<img src="https://media.giphy.com/media/OJbr914LLiETe/giphy.gif" alt="Beyoncé">`;
         cssclass = "linha1";
     }
     else if(resultado >= 18.5 && resultado < 25){
-        text = "Uma louca 🎵";
+        text = "O seu IMC é " + resultado.toFixed(2) + " - Uma louca 🎵";
         gif = `<img src="https://media.giphy.com/media/11wsZr0jbXc15m/giphy.gif" alt="Beyoncé">`;
         cssclass = "linha2";
     }
     else if(resultado >= 25 && resultado < 35){
-        text = "Uma feiticeira 🎵";
+        text = "O seu IMC é " + resultado.toFixed(2) + " - Uma feiticeira 🎵";
         gif = `<img src="https://media.giphy.com/media/BxrFUN7RB9k8o/giphy.gif" alt="Beyoncé">`;
         cssclass = "linha3";
     }
     else if(resultado >=35 && resultado < 40){
-        text = "Ela é demais 🎵";
+        text = "O seu IMC é " + resultado.toFixed(2) + " - Ela é demais 🎵";
         gif = `<img src="https://media.giphy.com/media/4F7OSamSWRBfi/giphy.gif" alt="Beyoncé">`;
         cssclass = "linha4";
     }
-    else{
-        text = "Meu deus, ela é demais 🎵";
+    else if(resultado >= 40){
+        text = "O seu IMC é " + resultado.toFixed(2) + " - Meu deus, ela é demais 🎵";
         gif = `<img src="https://media.giphy.com/media/mngbEYFPa3NbG/giphy.gif" alt="Beyoncé">`;
         cssclass = "linha5";
     }
+    else{
+        alert("Digite o seu peso apenas com números e a sua altura no formato x.xx")
+        text = "Não foi possível calcular o seu IMC"
+        gif = `<img src="https://media.giphy.com/media/aP6UZVDPzAlvW/giphy.gif" alt="Beyoncé">`;
+    }
 
-    resposta.innerHTML = "O seu IMC é " + resultado.toFixed(2) + " - " + text;
+    resposta.innerHTML = text;
     resposta.className = cssclass;
     lugarGif.innerHTML = gif;
 }
